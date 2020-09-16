54c0ce76cfe32252ce79252c3ef5ef5f3372b8cd
Greg Mitchell <g2mitchell3003@gmail.com>
2020-09-16 14:55:35 -0600

    Remove looping instance variable assignments in game class and fix tests for game class

--- lib/game.rb
+++ lib/game.rb
@@ -2,11 +2,16 @@ class Game
   attr_reader :game_id, :season, :type, :date_time, :away_team_id, :home_team_id, :away_goals,
               :home_goals, :venue, :venue_link
   def initialize(row)
-    row.each do |k, v|
-        instance_variable_set("@#{k}", v)
-    end
-    @away_goals = @away_goals.to_i
-    @home_goals = @home_goals.to_i
+    @game_id = row['game_id']
+    @season = row['season']
+    @type = row['type']
+    @date_time = row['date_time']
+    @away_team_id = row['away_team_id']
+    @home_team_id = row['home_team_id']
+    @away_goals = row['away_goals'].to_i
+    @home_goals = row['home_goals'].to_i
+    @venue = row['venue']
+    @venue_link = row['venue_link']
   end
 
   def game_info
