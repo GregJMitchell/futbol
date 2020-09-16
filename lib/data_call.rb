eb9f292c7a098d087bb3bb41b8b0598af637567c
Curtis Bartell <cbartell980@gmail.com>
2020-09-15 21:06:38 -0600

    Add games_by_team

--- lib/data_call.rb
+++ lib/data_call.rb
@@ -3,8 +3,14 @@ module DataCall
     @stat_tracker.team_manager.teams
   end
 
-  def game_ids_by_team(id)
+  def game_ids_by_team(team_id)
     game_teams.select { |game_team| game_team.team_id == id }.map(&:game_id)
+  end
+
+  def games_by_team(team_id)
+    games.select do |game|
+      game.home_team_id == team_id || game.away_team_id == team_id
+    end
   end
 
   def find_game_teams(game_id)
@@ -28,16 +34,16 @@ module DataCall
     games.find {|game| game.game_id == game_id }.game_info
   end
 
-  def game_ids_by_season(id)
-    gather_game_info(id).reduce({}) do |collector, game|
+  def game_ids_by_season(team_id)
+    gather_game_info(team_id).reduce({}) do |collector, game|
       collector[game[:season_id]] = [] if collector[game[:season_id]].nil?
       collector[game[:season_id]] << game[:game_id]
       collector
     end
   end
 
-  def game_teams_by_season(id)
-    seasons = game_ids_by_season(id)
+  def game_teams_by_season(team_id)
+    seasons = game_ids_by_season(team_id)
     seasons.each do |season, game_ids|
       seasons[season] = game_ids.map do |game_id|
         game_team_info(game_id)
